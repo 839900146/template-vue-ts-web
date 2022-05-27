0740f72a664b728b3baab4245adcabedec905647
19907766402 <839900146@qq.com>
2022-05-27 17:11:27 +0800

    配置eslint

---
+++ config/server.ts
@@ -0,0 +1,45 @@
+export default {
+	/**
+	 * token保存至localStorage中的键
+	 */
+	tokenKey: 'webapp-User-token',
+	/**
+	 * 设置在xhr headers中token的键
+	 */
+	tokenKeyInRequest: 'userToken',
+	/**
+	 * 不同环境下, 发送请求的前缀
+	 */
+	apiPrefix: {
+		dev: '/dev/api',
+		prd: '/prd/api',
+		test: '/test/api',
+	},
+	/**
+	 * 前端代理服务器
+	 */
+	proxyServer: {
+		server1: 'http://localhost:8001',
+		server2: 'http://localhost:8002',
+		server3: 'http://localhost:8003',
+		server4: 'http://localhost:8004',
+	},
+	/**
+	 * 静态资源存放目录
+	 */
+	assetDir: 'static',
+	/**
+	 * 静态资源路径前缀
+	 */
+	publicPath: '/',
+	/**
+	 * 网站二级目录
+	 */
+	base: '/web/',
+	/**
+	 * 需要注入的全局数据（编译时使用）
+	 */
+	injectData: {
+		...process.env, // 这一行不要删除，因为程序中有用到
+	},
+}
