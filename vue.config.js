ba72e2249f45a95fc740fb942e736278476463fa
19907766402 <839900146@qq.com>
2022-05-28 18:19:04 +0800

    工具: 增加alias路径映射
    新增assets, router, views, components的路径映射

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
 const server = require('./config/server.ts')
 const proxys = require('./config/proxy.ts')
 module.exports = defineConfig({
@@ -8,5 +9,13 @@ module.exports = defineConfig({
 	productionSourceMap: false,
 	devServer: {
 		proxy: proxys,
+	},
+	chainWebpack: (config) => {
+		config.resolve.alias
+			.set('@', path.join(__dirname, './src'))
+			.set('@router', path.join(__dirname, './src/router'))
+			.set('@views', path.join(__dirname, './src/views'))
+			.set('@assets', path.join(__dirname, './src/assets'))
+			.set('@comp', path.join(__dirname, './src/components'))
 	},
 })
