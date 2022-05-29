b3218627e4bd0d85d36e39a3213a34525030b9fe
19907766402 <839900146@qq.com>
2022-05-29 20:04:26 +0800

    工具(vue.config.js): 增加gzip压缩

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 const path = require('path')
+const CompressionWebpackPlugin = 'compression-webpack-plugin'
 const server = require('./config/server.ts')
 const proxys = require('./config/proxy.ts')
 module.exports = defineConfig({
@@ -9,6 +10,26 @@ module.exports = defineConfig({
 	productionSourceMap: false,
 	devServer: {
 		proxy: proxys,
+	},
+	configureWebpack: {
+		cache: {
+			type: 'filesystem',
+		},
+		optimization: {
+			minimize: true,
+			splitChunks: {
+				chunks: 'all',
+				minSize: 30000,
+				minChunks: 2,
+				maxAsyncRequests: 30,
+				maxInitialRequests: 30,
+				enforceSizeThreshold: 50000,
+			},
+		},
+		// 路径查找优化
+		resolve: {
+			modules: [path.resolve(__dirname, './src'), path.resolve(__dirname, './node_modules')],
+		},
 	},
 	chainWebpack: (config) => {
 		config.resolve.alias
@@ -20,5 +41,17 @@ module.exports = defineConfig({
 			.set('@comp', path.join(__dirname, './src/components'))
 			.set('@store', path.join(__dirname, './src/store'))
 			.set('@utils', path.join(__dirname, './src/utils'))
+
+		if (process.env.NODE_ENV === 'production') {
+			config.plugin('compression-webpack-plugin').use(CompressionWebpackPlugin, [
+				{
+					filename: '[path][base].gz',
+					algorithm: 'gzip',
+					test: new RegExp('\\.(js|css)$'),
+					threshold: 20480,
+					minRatio: 0.6,
+				},
+			])
+		}
 	},
 })
