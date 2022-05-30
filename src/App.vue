41fa543c81a1f3dff0f3aab84db031f0ad58d0ba
19907766402 <839900146@qq.com>
2022-05-30 11:41:29 +0800

    工具(package.json): 删除不再需要的stylelint-config-standard

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,11 @@
 
 <style lang="scss">
 nav {
-	position: absolute;
 	padding: 30px;
 
 	a {
 		color: #2c3e50;
 		font-weight: bold;
-		opacity: 0.9;
 
 		&.router-link-exact-active {
 			color: #42b983;
